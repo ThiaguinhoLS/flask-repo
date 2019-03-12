81e68c85ee8a1560a33bbaf78cbcb581c351ebb9
ThiaguinhoLS <tthiaguinho638@gmail.com>
2019-03-12 03:26:27 -0300

    Authentication and email confirmation

--- authentication-with-email-confirm/config.py
+++ authentication-with-email-confirm/config.py
@@ -9,11 +9,11 @@ class Config(object):
         or "sqlite:///" + os.path.join(BASE_DIR, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "S3CR3T-K3Y"
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") or "tthiaguinho638@gmail.com"
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or "654750321529456"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_SERVER = os.environ.get("MAIL_SERVER") or "smtp.gmail.com"
     MAIL_PORT = os.environ.get("MAIL_PORT") or 465
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") or False
     MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL") or True
-    MAIL_DEFAULT_SENDER = "tthiaguinho638@gmail.com"
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
 
